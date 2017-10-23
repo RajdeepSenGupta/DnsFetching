31dc898b6ace308bdd388615c70fe8f12fce1500
Rajdeep <rajdeep@promactinfo.com>
2017-10-23 11:36:16 +0530

    Initial Commit

---
+++ DnsFetching/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace DnsFetching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a url: ");
+                string dns = Console.ReadLine();
+                IPHostEntry hostInfo = Dns.GetHostEntry(dns);
+                Console.Write("\nCorresponding dns address: ");
+                Console.WriteLine(hostInfo.AddressList[0]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
